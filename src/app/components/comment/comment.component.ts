91c10bb7573a129e5a5eb67137fee26f7b77f1a8
Matthieu <Matthieu.Maginot@gmail.com>
2023-07-21 09:37:37 +0200

    lenght to length

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -9,18 +9,18 @@ import { IComment } from 'src/app/utils/interface';
 export class CommentComponent implements OnChanges {
   @Input() serieId = '';
   comments: IComment[] = [];
-  commentsLenght = 0;
+  commentsLength = 0;
   constructor(public librariesService: LibrariesService) {}
   ngOnChanges(): void {
     this.librariesService.getComments(this.serieId).subscribe((data) => {
       this.comments = data;
       this.comments.map((comment) => (comment.score = Number(comment.score)));
-      this.commentsLenght = this.comments.length;
+      this.commentsLength = this.comments.length;
       if (
         this.comments.some((comment) => comment.score === null) ||
         this.comments.some((comment) => comment.comment === null)
       ) {
-        this.commentsLenght -= 1;
+        this.commentsLength -= 1;
       }
     });
   }
