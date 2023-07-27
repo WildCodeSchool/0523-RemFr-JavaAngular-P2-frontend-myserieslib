1e3f847aaecf65952713689da9b1d4df7fc0f0ee
TRINH <laser2u02@gmail.com>
2023-07-27 09:35:33 +0200

    fix bug console

--- src/app/components/dashboard/table/category-table/category-table.component.ts
+++ src/app/components/dashboard/table/category-table/category-table.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { ICategories } from 'src/app/utils/interface';
 import { ModalService } from 'src/app/services/modal/modal.service';
 import { CategoriesService } from 'src/app/services/categories/categories.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-category-table',
@@ -12,7 +13,11 @@ export class CategoryTableComponent {
   @Input() categories: ICategories[] = [];
   @Output() updateCategoryEvent = new EventEmitter<ICategories>();
 
-  constructor(private modalService: ModalService, private categoriesService: CategoriesService) {}
+  constructor(
+    private modalService: ModalService,
+    private categoriesService: CategoriesService,
+    private toastr: ToastrService
+  ) {}
 
   deleteCategory(category: ICategories): void {
     this.categoriesService.deleteCategory(category.id).subscribe(
@@ -20,7 +25,7 @@ export class CategoryTableComponent {
         this.updateCategoryEvent.emit();
       },
       (error) => {
-        console.error('Error deleting category:', error);
+        this.toastr.error(error.error.message);
       }
     );
   }
