338e0c9510de42de5708baa1c7ff79a0de6bf77d
Ludivine <sagnol.ludivine@gmail.com>
2023-07-27 17:06:54 +0200

    fix(modal): fix modal coming back after login && showing add button on header when disconnected

--- src/app/components/UI/btn-add/btn-add.component.ts
+++ src/app/components/UI/btn-add/btn-add.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { LibrariesService } from 'src/app/services/libraries/libraries.service';
-
+import { ModalService } from 'src/app/services/modal/modal.service';
+import { ToastrService } from 'ngx-toastr';
+import { Store } from '@ngrx/store';
+import { selectUser } from 'src/app/services/store/user.reducer';
 @Component({
   selector: 'app-btn-add',
   templateUrl: './btn-add.component.html',
@@ -8,21 +12,40 @@ import { LibrariesService } from 'src/app/services/libraries/libraries.service';
 })
 export class BtnAddComponent implements OnInit {
   @Input() id = '';
-  isAlreadyInLibrary = true;
+  isAlreadyInLibrary = false;
 
-  constructor(private libraryService: LibrariesService) {}
+  constructor(
+    private libraryService: LibrariesService,
+    private router: Router,
+    private modalService: ModalService,
+    private toaster: ToastrService,
+    private store: Store
+  ) {}
+
+  user: any = {};
 
   ngOnInit(): void {
-    this.libraryService.getLibraries().subscribe((data) => {
-      this.isAlreadyInLibrary = data.some((library) => library.serie.id === this.id);
+    this.store.select(selectUser).subscribe((user) => {
+      this.user = user;
     });
+    if (this.user.nickname) {
+      this.libraryService.getLibraries().subscribe((data) => {
+        this.isAlreadyInLibrary = data.some((library) => library.serie.id === this.id);
+      });
+    }
   }
 
   addSerieToLibrary() {
-    if (!this.isAlreadyInLibrary) {
-      this.libraryService.addSeries(this.id).subscribe(() => {
-        this.isAlreadyInLibrary = true;
-      });
-    }
+    this.modalService.openModal(() => {
+      if (!this.isAlreadyInLibrary) {
+        this.libraryService.addSeries(this.id).subscribe(
+          () => {
+            this.isAlreadyInLibrary = true;
+            this.toaster.success('Serie ajoutée à votre bibliothèque');
+          },
+          () => this.toaster.error('Une erreur est survenue')
+        );
+      }
+    });
   }
 }
